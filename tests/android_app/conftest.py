2a862d34ae2f0056c11dc652610e17a80f136364
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-07-26 16:24:57 +0300

    hw19

--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -20,8 +20,8 @@ def mobile_management():
 
     options = UiAutomator2Options().load_capabilities({
         "platformName": "android",
-        "platformVersion": "9.0",
-        "deviceName": "Google Pixel 3",
+        "platformVersion": "12.0",
+        "deviceName": "Google Pixel 6",
 
         # Set URL of the application under test
         "app": "bs://sample.app",
